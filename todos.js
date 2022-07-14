dc807fc51202ad0a7d93b970c76d71f31631ff4f
Igor <48488039+hjphopchop@users.noreply.github.com>
2022-07-14 16:01:14 +0300

    feat: added todos data

---
+++ todos.js
@@ -0,0 +1,62 @@
+export const todos = [
+  {
+    id: "1",
+    date: "14.07.22",
+    author: "Igor",
+    message: "сходить в аптеку",
+  },
+  {
+    id: "2",
+    date: "15.07.22",
+    author: "Igor",
+    message: "сходить в магазин",
+  },
+  {
+    id: "3",
+    date: "19.08.22",
+    author: "Igor",
+    message: "баскет",
+  },
+  {
+    id: "4",
+    date: "19.07.22",
+    author: "Igor",
+    message: "чтение",
+  },
+  {
+    id: "5",
+    date: "15.07.22",
+    author: "Igor",
+    message: "позвонить сестре",
+  },
+  {
+    id: "6",
+    date: "19.07.22",
+    author: "Igor",
+    message: "выйти на работу",
+  },
+  {
+    id: "7",
+    date: "18.07.22",
+    author: "Igor",
+    message: "купить корм собаке",
+  },
+  {
+    id: "8",
+    date: "20.07.22",
+    author: "Igor",
+    message: "TODO-Effector-app",
+  },
+  {
+    id: "9",
+    date: "21.07.22",
+    author: "Igor",
+    message: "Авторизация и API Next.js",
+  },
+  {
+    id: "10",
+    date: "22.07.22",
+    author: "Igor",
+    message: "Приложение для обзора аналитики",
+  },
+];
