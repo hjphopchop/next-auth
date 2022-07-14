a7df23bb4d50a2c53944dba15bc536d3efdb9d87
Igor <48488039+hjphopchop@users.noreply.github.com>
2022-07-14 17:23:32 +0300

    feat: added loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 300,
+  showSpinner: true,
+});
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
